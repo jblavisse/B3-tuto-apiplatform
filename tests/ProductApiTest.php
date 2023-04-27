e0389c5947b5372e0432c4e82b0d265ab3edc866
Jb <jb@levillageducode.fr>
2023-04-27 14:32:23 +0200

    Comments for Get product API

--- tests/ProductApiTest.php
+++ tests/ProductApiTest.php
@@ -16,5 +16,19 @@ class ProductApiTest extends ApiTestCase
         $this->assertResponseIsSuccessful();
         $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
         $this->assertCount(10, $response->toArray());
+
+        // Mettre les produits dans une propriété privée
+    }
+
+    public function testGet(): void
+    {
+        // Exécuter la fonction testGetAll
+        // Récupérer le premier produit du tableau
+
+        // Faire une requête pour récupérer avec l'identifiant de ce premier produit
+        // Vérifier l'en-tête de la réponse
+        // Regarder dans le json pour voir ce qu'il CONTIENT
+        // le titre du produit numéro 1 correspond bien au titre récupérée dans le tableau
+        // ...
     }
 }
